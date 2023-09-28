17ffc85f78fc9c9d4ae3243b7b62eb1631d6f7e7
arisheh <a_icam@yahoo.com>
2023-09-27 17:33:01 -0700

    feat: all except terminate

--- main.py
+++ main.py
@@ -33,12 +33,15 @@ servers = []
 def handle_client(client_socket, client_address):
     print(f"Accepted connection from {client_address}")
     global clients
-    clients.append(client_address)
+    clients.append(client_socket)
 
     global server_running
     while server_running:
         # Receive data from the client
-        data = client_socket.recv(1024)
+        try:
+            data = client_socket.recv(1024)
+        except ConnectionAbortedError:
+            break
         if not data:
             break
         # Print the received data and sender's IP address
@@ -62,12 +65,49 @@ def setup_server():
         except OSError:
             print("All incoming connections terminated!")
 
+def connect_server(address, port):
+
+    global servers
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect to the server
+    client_socket.connect((address, port))
+    servers.append({"socket_obj": client_socket, "address": address, "port": port})
+    print(f"Successfully connected to {address}:{port}")
+
+def list_servers():
+    global servers
+    for i, server in enumerate(servers):
+        print(f"{i}: {server['address']}:{server['port']}")
+
+def send_message(id, msg):
+    global servers
+    servers[id]['socket_obj'].send(msg.encode('utf-8'))
+
+def terminate_all_clients():
+    global clients
+    for client in clients:
+        client.close()
+
+
 server_handler = threading.Thread(target=setup_server)
 server_handler.start()
 
 while True:
     command = input("Command: ")
+    if command.__contains__("connect"):
+        _, dst, port = command.split(' ')
+        print(f"Trying to connect to {dst}:{port}")
+        connect_server(dst, int(port))
+    if command.__contains__("send"):
+        _, id, msg = command.split(' ')
+        print(f"Sending message to id {id}")
+        send_message(int(id), msg)
+    if command == "list":
+        list_servers()
+
     if command == "exit":
+        terminate_all_clients()
         server_running = False
         server_socket.close()
         server_handler.join()
